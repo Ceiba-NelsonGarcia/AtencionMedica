deca3429c417148ad0edeb11b1cb82f49de8c64d
Ceiba-NelsonGarcia <nelson.garcia@ceiba.com.co>
2022-04-06 07:35:41 -0500

    48 Se crear Test RepositorioCitaMysqlTest

--- java-arquitectura-hexagonal/microservicio/infraestructura/src/test/java/com/ceiba/cita/adaptador/repositorio/RepositorioCitaMysqlTest.java
+++ java-arquitectura-hexagonal/microservicio/infraestructura/src/test/java/com/ceiba/cita/adaptador/repositorio/RepositorioCitaMysqlTest.java
@@ -1,7 +1,6 @@
 package com.ceiba.cita.adaptador.repositorio;
 
 import com.ceiba.cita.modelo.entidad.Cita;
-import com.ceiba.cita.puerto.repositorio.RepositorioCita;
 import com.ceiba.infraestructura.jdbc.CustomNamedParameterJdbcTemplate;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,7 +11,7 @@ import java.time.LocalDate;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
-class RepositorioCitaTest {
+class RepositorioCitaMysqlTest {
 
     Cita cita = new Cita(1L, 1L,1L, LocalDate.now(), 8, 9, 50D, 185000D);
 
@@ -25,7 +24,7 @@ class RepositorioCitaTest {
         // act
         String sqlCrear = "insert into cita (id_usuario,id_doctor,fecha_cita,hora_inicial,hora_final,valor_usd, valor_cop) values(:idUsuario,:idDoctor,:fechaCita,:horaInicial,:horaFinal,:valorUsd,:valorCop)";
         Mockito.when(customNamedParameterJdbcTemplate.crear(cita,sqlCrear)).thenReturn(1L);
-        /**TODO*/
+        /**TODO no pasa por bien por customNamedParameterJdbcTemplate*/
         // assert
         assertEquals(0L, repositorioCitaMysql.crearCita(cita));
     }
